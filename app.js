8c7ef1c6b5896ca15f5f67604f12ee7122eb7620
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 22:24:54 -0400

    add highlight styling on click in nameInput field, remove when focus is gone

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const nameInput = document.getElementById('name')
 const messageTextArea = document.getElementById('message')
+
+//addEventListener format
+//element.addEventListener(eventType, callback)
+
+//'click' only works for mouse, for keyboard inputs, use 'focus'
+nameInput.addEventListener('focus', (e) => {
+  e.target.className = 'highlight'
+})
+
+//use 'blur' for focus leaving element
+nameInput.addEventListener('blur', (e) => {
+  e.target.className = ''
+})
+
+//
